92c4bfc91ed5dc7551a432fab906b3cc46d0a9bb
tyws <727250781@qq.com>
2015-11-19 08:55:33 +0800

    Update settings.py

--- Test/Test/settings.py
+++ Test/Test/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+#change1
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
